ec4791e22f9e317a843079d4f695dbd7f44b2603
skarcot <sathishkumar.arcot@gmail.com>
2016-10-16 18:42:07 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,11 +90,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+  res.send(createTemplate(articles.articleTwo));
 });
 
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+  res.send(createTemplate(articles.articleThree));
 });
 
 app.get('/ui/style.css', function (req, res) {
