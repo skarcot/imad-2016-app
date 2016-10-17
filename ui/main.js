cbc7b8743a04f5d1d9e2d5d46834a375b813c4ea
skarcot <sathishkumar.arcot@gmail.com>
2016-10-17 19:37:44 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,18 @@
+// counter code:
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function() {
+// make a req to the counter endpoint
+
+//capture the resp & store it in a var
+
+//render the var in the correct span
+var span = document.getElementById('count');
+counter++;
+span.innerHtml = counter.toString();
+};
+
+/*
 console.log('Loaded!');
 
 // change the text of the  main-text div
@@ -16,3 +31,4 @@ img.onclick = function()
 {
     var interval = setInterval(moveRight, 50);
 };
+*/
