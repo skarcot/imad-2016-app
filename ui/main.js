d78ff5aed169f96559fb5696d6258c51bdd667b1
skarcot <sathishkumar.arcot@gmail.com>
2016-10-16 23:24:21 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,12 @@
 console.log('Loaded!');
 
+// change the text of the  main-text div
 var element = document.getElementById('main-text');
-
 element.innerHTML='changed main text from main js';
+
+// move the image madi
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+  img.style.marginLeft = '100px';
+};
