00bcacdea47c8dad29ec3e9d714b772ac292eba9
skarcot <sathishkumar.arcot@gmail.com>
2016-10-17 21:25:49 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,28 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
-// make a req to the counter endpoint
-
-//capture the resp & store it in a var
-
-//render the var in the correct span
-counter++;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+    // create a req object
+    reqCounter = XMLHttpRequest();
+    
+    // capture the resp & store it in a var
+    // check whether the req's on ready state has changed?
+    reqCounter.onreadystatechange = function(){
+        // check whether req's ready state is DONE(it cud be - unsent, opened, headers-received, loading, done)
+        if(reqCounter.readyState == XMLHttpRequest.DONE) {
+            // check whether response code is OK(200)
+            if(reqCounter.state == 200) {
+                var counter = reqCounter.responseText;
+                //render the var in the correct span
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // not done yet
+    };
+    
+    // make the req to the counter endpoint
+    reqCounter.open('GET', 'http://skarcot.imad.hasura-app.io/counter', true);
+    reqCounter.send();
 };
 
 /*
